f2a30aeeaece1c10264c9a345e0a1b99ac14b2e3
Jose Guillermo <30608715+jgcuelap17722@users.noreply.github.com>
2022-07-09 21:50:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 // Routes import
@@ -9,6 +10,7 @@ import welcome from "./src/routes/routes.js"
 //middleware
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 //use routes
 app.use("/api/v1.0", welcome);
 app.use("/api/v1.0", countries);
